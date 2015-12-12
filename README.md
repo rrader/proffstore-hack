871f2f0b1d98a538b4d5cf63c4ae3a4d56eb12c8
Roman Rader <roman.rader@gmail.com>
2015-12-12 23:54:56 +0200

    config

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # proffstore-hack
+
+curl -v https://api.github.com/repos/kpi-petitions/project-y/commits/master/statuses
+
+$ ssh pi@139.96.30.142 -R 9999:localhost:9999 "ssh -D 9050 roma@139.96.30.100"
+
+
+
--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-import RPi.GPIO as GPIO
+import configparser
 
-GREEN = 20
-YELLOW = 26
+config_ = None
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(GREEN, GPIO.OUT)
-GPIO.setup(YELLOW, GPIO.OUT)
+def config():
+    global config_
+    if config_ is None:
+        config_ = configparser.ConfigParser()
+        config_.read('config.ini')
+    return config_
 
-GPIO.output(YELLOW, True)
+
+def main():
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(GREEN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(YELLOW, GPIO.OUT)
+
+    GPIO.output(YELLOW, GPIO.HIGH)
+    GPIO.output(GREEN, GPIO.HIGH)
+
+    GPIO.output(GREEN, GPIO.LOW)
+
+
+if __name__ == "__main__":
+    # main()
+    print(config().sections())
