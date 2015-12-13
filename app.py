f8e4f8fa9e88c72731ceb7d8affc6102bfc13db7
Roman Rader <roman.rader@gmail.com>
2015-12-13 05:00:30 +0200

    integration with github

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import socks # you need to install pysocks (see above)
 import configparser
 
 config_ = None
+state_ = 'pending'
+blink_status_ = False
 
 def config():
     global config_
@@ -15,28 +17,67 @@ socks.set_default_proxy(socks.SOCKS5, config()['proxy']['host'], int(config()['p
 socket.socket = socks.socksocket
 
 import requests
-from github import Github
+from time import sleep
+import threading
+import RPi.GPIO as GPIO
 
 
+GREEN = int(config()['GPIO']['GREEN'])
+RED = int(config()['GPIO']['RED'])
+YELLOW = int(config()['GPIO']['YELLOW'])
 
-def main():
-    import RPi.GPIO as GPIO
+
+def setup_gpio():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(GREEN, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(YELLOW, GPIO.OUT)
+    GPIO.setup(GREEN, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(RED, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(YELLOW, GPIO.OUT, initial=GPIO.HIGH)
 
-    GPIO.output(YELLOW, GPIO.HIGH)
-    GPIO.output(GREEN, GPIO.HIGH)
 
-    GPIO.output(GREEN, GPIO.LOW)
+def test(session):
+    r = session.get("https://api.github.com/repos/{}/{}/commits/{}/statuses".format(
+            config()['remote']['user'], config()['remote']['repo'], config()['remote']['ref']
+        ),
+        auth=(config()['github']['username'], config()['github']['key'])
+        )
+    j = r.json()
+    print(j)
+    if j:
+        return j[0]['state']
+
+
+def blink_worker():
+    global state_, blink_status_
+    while True:
+        print('Blinking {}'.format(state_))
+        blink_status_ = not blink_status_
+        if state_ == 'success':
+            GPIO.output(GREEN, blink_status_)
+            GPIO.output(YELLOW, GPIO.LOW)
+            GPIO.output(RED, GPIO.LOW)
+        elif state_ == 'pending':
+            GPIO.output(GREEN, GPIO.LOW)
+            GPIO.output(YELLOW, blink_status_)
+            GPIO.output(RED, GPIO.LOW)
+        elif state_ == 'failure':
+            GPIO.output(GREEN, GPIO.LOW)
+            GPIO.output(YELLOW, GPIO.LOW)
+            GPIO.output(RED, blink_status_)
+        if blink_status_:
+            sleep(1)
+        else:
+            sleep(2)
 
 
 if __name__ == "__main__":
-    print(config().sections())
+    setup_gpio()
+
+    t = threading.Thread(target=blink_worker) 
+    t.start()
+
     s = requests.Session()
-    gh = Github(config()['github']['key'])
-    repo = gh.get_repo('{}/{}'.format(config()['remote']['user'], config()['remote']['repo']))
-    branch = repo.get_branch(config()['remote']['ref'])
-    print(branch.commit.get_statuses())
-    # r = s.get("https://api.github.com/repos/kpi-petitions/project-y/commits/master/statuses")
-    # print(r.text)
+    while True:
+        new_state = test(s)
+        if new_state:
+            state_ = new_state
+        sleep(3)
